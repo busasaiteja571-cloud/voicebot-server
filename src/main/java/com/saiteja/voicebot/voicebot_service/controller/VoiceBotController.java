bba76e0aac1c8bd2fdcce51c6e3d09253aaa1997
busasaiteja571-cloud <busasaiteja571@gmail.com>
2026-06-27 19:57:30 +0530

    Add health endpoint

--- src/main/java/com/saiteja/voicebot/voicebot_service/controller/VoiceBotController.java
+++ src/main/java/com/saiteja/voicebot/voicebot_service/controller/VoiceBotController.java
@@ -2,6 +2,7 @@ package com.saiteja.voicebot.voicebot_service.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +22,11 @@ public class VoiceBotController {
     // Standard constructor dependency injection
     public VoiceBotController(GeminiService geminiService) {
         this.geminiService = geminiService;
+    }
+
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("ok");
     }
 
     @PostMapping("/chat")
